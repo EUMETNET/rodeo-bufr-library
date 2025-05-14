c679930597d290cfd16abc49af2658fb26b394b4
istvans <istvans@met.no>
2025-05-14 16:23:12 +0200

    Add WMO tables downloader

---
+++ bufr/tables/wmo/get_wmo_tables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Updating WMO BUFR table files
+
+RELEASE_NUM=44
+SRC_URL="https://github.com/wmo-im/BUFR4/archive/refs/tags/v${RELEASE_NUM}.zip"
+
+DL_SRC="./WMO_BUFR_tables_v${RELEASE_NUM}.zip"
+
+if [ ! -f $DL_SRC ]
+then
+    wget $SRC_URL -O ${DL_SRC}
+fi
+unzip -j -o ${DL_SRC} "BUFR4-${RELEASE_NUM}/txt/BUFR*.txt" -d ./
