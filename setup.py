1d0bcbdaa40657005824ad969e242fe406a5ccd5
Amund Isaksen <amundi@met.no>
2025-06-05 16:10:33 +0200

    Remove unvanted files from build

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ from glob import glob
 
 __version__ = "0.0.1"
 
+exclude_files_from_build = [
+    "src/bufr_tools/bufresohmsgfrenc.cpp",
+    "src/bufr_tools/covjson2bufr_main.cpp",
+    "src/bufr_tools/bufresohmsg.cpp",
+    "src/bufr_tools/bufrenc.cpp",
+]
+
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
 # * You can set include_pybind11=false to add the include directory yourself,
@@ -17,7 +24,13 @@ __version__ = "0.0.1"
 ext_modules = [
     Pybind11Extension(
         "bufr_tools",
-        sorted(glob("src/bufr_tools/*.cpp")),
+        sorted(
+            [
+                i
+                for i in glob("src/bufr_tools/*.cpp")
+                if i not in exclude_files_from_build
+            ]
+        ),
         # Example: passing in the version to the compiled code
     ),
 ]
