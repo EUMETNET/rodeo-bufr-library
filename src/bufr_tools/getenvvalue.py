d1e7756ad27a6f9255068125ae5081dc959a2305
istvans <istvans@met.no>
2025-12-01 17:11:42 +0100

    Fix typo

--- src/bufr_tools/getenvvalue.py
+++ src/bufr_tools/getenvvalue.py
@@ -52,7 +52,7 @@ def getRadarCFDir() -> str:
     return getPackageRootDir() + "/data/radar/radar_cf.py"
 
 
-def geBufrRadarTableDir() -> str:
+def getBufrRadarTableDir() -> str:
     """
     Get BUFR table directory from environment variable or from package.
     """
