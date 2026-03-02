9db5194f02979eee9d4215d6e437f651e5638346
istvans <istvans@met.no>
2026-03-02 13:27:47 +0100

    arm64-mac build

--- setup.py
+++ setup.py
@@ -16,8 +16,12 @@ exclude_files_from_build = [
 common_extra_compile_args = []
 linux_extra_compile_args = ["--std=c++17"]
 windows_extra_compile_args = ["-IC:\\temp\\rapidjson\\rapidjson-1.1.0\\include", "/std:c++17"]
+macos_extra_compile_args = ["-I/tmp/rapidjson-1.1.0/include", "--std=c++17"]
+
 if sys.platform == "win32":
     extra_compile_args_for_extension = common_extra_compile_args + windows_extra_compile_args
+elif sys.platform == "darwin":
+    extra_compile_args_for_extension = common_extra_compile_args + macos_extra_compile_args
 else:
     extra_compile_args_for_extension = common_extra_compile_args + linux_extra_compile_args
 
