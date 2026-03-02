d09b046357722ca8565cbaae2067b5c02bd488be
istvans <istvans@met.no>
2026-03-02 14:43:45 +0100

    Using SetInt at content["size"]

--- src/bufr_tools/ESOHBufr.cpp
+++ src/bufr_tools/ESOHBufr.cpp
@@ -1311,7 +1311,7 @@ bool ESOHBufr::addContent(const Descriptor &v, std::string cf_name,
     mvalue.SetString(value_str.c_str(), message_allocator);
   }
   content["value"] = mvalue;
-  content["size"] = value_str.size();
+  content["size"].SetString(std::to_string(value_str.size()).c_str(), message_allocator);
   content["standard_name"].SetString(cf_name.c_str(), message_allocator);
   if (meta) {
     if (meta->unit() == "CODE TABLE") {
