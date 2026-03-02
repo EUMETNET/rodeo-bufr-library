16060a56de4b560c951569957a98c9476fd311b8
Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>
2026-03-02 10:53:14 +0100

    Update setup.py for macos

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 from glob import glob
 import sys
+import os
 
 # __version__ = "0.0.1"
 
@@ -16,8 +17,11 @@ exclude_files_from_build = [
 common_extra_compile_args = []
 linux_extra_compile_args = ["--std=c++17"]
 windows_extra_compile_args = ["-IC:\\temp\\rapidjson\\rapidjson-1.1.0\\include", "/std:c++17"]
+macos__extra_compile_args = [f"-I{os.getenv("GITHUB_WORKSPACE")}/include", "--std=c++17"]
 if sys.platform == "win32":
     extra_compile_args_for_extension = common_extra_compile_args + windows_extra_compile_args
+elif sys.platfrom == "darwin":
+    extra_compile_args_for_extension = common_extra_compile_args + macos_extra_compile_args
 else:
     extra_compile_args_for_extension = common_extra_compile_args + linux_extra_compile_args
 
